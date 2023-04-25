eb4c3ddeff8cd5ba22dd1f47064bb07fed840592
Jasur <jasafayz01@gmail.com>
2023-04-25 19:33:42 +0500

    feat: Lazy loadinf added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Suspense } from 'react'
 import Navbar from './navbar/Navbar'
 import './app.css'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
-import Registration from './authorization/Registration'
-import Login from './authorization/Login'
-import NotFound from './nooFound/NotFound'
 import { useDispatch, useSelector } from 'react-redux'
 import { auth } from '../actions/user'
-import User from './User/User'
-import PostsWrapper from './Posts/PostsWrapper'
-import PostDescription from './Posts/PostDescription'
-import MenuCard from './AdminPanel/MenuCard'
 import useLocalstorage from '../hooks/use-localstorage'
 import i18n from '../i18n'
-import AddPost from './Posts/AddPost'
+import Spinner from '../utils/Spinner/Spinner'
 
 function App() {
 	const isAuth = useSelector((state) => state.user.isAuth)
@@ -41,6 +34,17 @@ function App() {
 		}
 	}
 
+	const Registration = React.lazy(() =>
+		import('./authorization/Registration')
+	)
+	const Login = React.lazy(() => import('./authorization/Login'))
+	const PostsWrapper = React.lazy(() => import('./Posts/PostsWrapper'))
+	const PostDescription = React.lazy(() => import('./Posts/PostDescription'))
+	const NotFound = React.lazy(() => import('./nooFound/NotFound'))
+	const User = React.lazy(() => import('./User/User'))
+	const AddPost = React.lazy(() => import('./Posts/AddPost'))
+	const MenuCard = React.lazy(() => import('./AdminPanel/MenuCard'))
+
 	return (
 		<BrowserRouter>
 			<Navbar
@@ -49,50 +53,65 @@ function App() {
 				language={language}
 			/>
 			<main className='px-4 mt-[70px] lg:px-0 h-full'>
-				{!isAuth && (
-					<Switch>
-						<Route path='/' exact>
-							<Redirect to='/all' />
-						</Route>
-						<Route path='/all'>
-							<PostsWrapper />
-						</Route>
-						<Route path='/registration' component={Registration} />
-						<Route path='/login' component={Login} />
+				<Suspense
+					fallback={
+						<div className='flex justify-center'>
+							<Spinner />
+						</div>
+					}
+				>
+					{!isAuth && (
+						<Switch>
+							<Route path='/' exact>
+								<Redirect to='/all' />
+							</Route>
+							<Route path='/all'>
+								<PostsWrapper />
+							</Route>
+							<Route path='/registration'>
+								<Registration />
+							</Route>
+							<Route path='/login'>
+								<Login />
+							</Route>
+							<Route path='/postinfo'>
+								<PostDescription />
+							</Route>
+							<Route exact path='*'>
+								<NotFound />
+							</Route>
+						</Switch>
+					)}
 
-						<Route path='/postinfo'>
-							<PostDescription />
-						</Route>
-						<Route exact path='*' component={NotFound} />
-					</Switch>
-				)}
+					{isAuth && (
+						<Switch>
+							<Route path='/' exact>
+								<Redirect to='/all' />
+							</Route>
+							<Route path='/all'>
+								<PostsWrapper />
+							</Route>
+							<Route path={'/user'}>
+								<User />
+							</Route>
+							<Route path={'/addpost'}>
+								<AddPost />
+							</Route>
+							<Route path='/postinfo'>
+								<PostDescription />
+							</Route>
+							{isAdmin && (
+								<Route path={'/admin'}>
+									<MenuCard />
+								</Route>
+							)}
 
-				{isAuth && (
-					<Switch>
-						<Route path='/' exact>
-							<Redirect to='/all' />
-						</Route>
-						<Route path='/all'>
-							<PostsWrapper />
-						</Route>
-						<Route path={'/user'}>
-							<User />
-						</Route>
-						<Route path={'/addpost'}>
-							<AddPost />
-						</Route>
-						<Route path='/postinfo'>
-							<PostDescription />
-						</Route>
-						{isAdmin &&
-						 <Route path={'/admin'}>
-							<MenuCard />
-						</Route> }
-
-						<Route path='*' component={NotFound} />
-					</Switch>
-				)}
-
+							<Route path='*'>
+								<NotFound />
+							</Route>
+						</Switch>
+					)}
+				</Suspense>
 			</main>
 		</BrowserRouter>
 	)
